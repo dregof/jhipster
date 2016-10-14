d991f4ae0630b0abdc8f652993f970708b90e43d
zeuser66 <zeuser66@gmail.com>
2016-10-14 11:25:10 +0200

    initial commit

---
+++ src/main/java/org/jhipster/health/app/config/liquibase/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Liquibase specific code.
+ */
+package org.jhipster.health.app.config.liquibase;
---
+++ src/main/java/org/jhipster/health/app/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package org.jhipster.health.app.security;
+
+import org.jhipster.health.app.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return (userName != null ? userName : Constants.SYSTEM_ACCOUNT);
+    }
+}
